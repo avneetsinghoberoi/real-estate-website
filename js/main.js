a13cc23d50e6d5b0cc9b42c9860150bb9e51e526
avneetsinghoberoi <avneetoberoi739@gmail.com>
2025-12-24 15:01:36 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function toggleMenu() {
+  document.querySelector(".nav-links")
+    .classList.toggle("active");
+}
+const form = document.getElementById("contactForm");
+const statusText = document.getElementById("formStatus");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  statusText.textContent = "Sending...";
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbx6uUR5gAG4jFq8pSkx_KsE2KI6jSOdLMB3Ejwl1iPXkfy_W_wtIUP0FTpx-M9FRuxYmw/exec", {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+      statusText.textContent = "Thank you! We will contact you shortly.";
+      form.reset();
+    } else {
+      statusText.textContent = "Something went wrong. Try again.";
+    }
+  } catch (error) {
+    statusText.textContent = "Error submitting form.";
+  }
+});
